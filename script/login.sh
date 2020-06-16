0ec3e0c7f02da1bbdcdca6b51cde5c44a2501054
12bbf7608ae1 <33011786+pdxgf1208@users.noreply.github.com>
2020-06-16 14:15:20 +0800

    Update login.sh

--- script/login.sh
+++ script/login.sh
@@ -76,10 +76,6 @@ week_ok=1
     esac
 # 登录id获取结束
 
-# exit 1
-
-# userip="10.130.194.44"
-
 # 自动参数获取  (根据url跳转实现)
     return_url=$(curl -Ss -H "User-Agent:${user_agent}" http://www.baidu.com | tr '\n\r' ' ' | sed -e 's/.*a href=\"//g' -e 's/\".*//g')
     hostname=$(echo ${return_url} | sed -e 's/http:\/\///g' -e 's/\/.*//g')  #获取登录用服务器ip
@@ -96,6 +92,13 @@ week_ok=1
     # wlanacname="SPL-BRAS-SR8806-X"  #备用
 
 # 其他请求参数设置结束
+
+# 检测参数是否成功获取
+    if [ -z "$hostname" ] || [ -z "$userip" ] || [ -z "$wlanacip" ] || [ -z "$wlanacname" ];then
+        echo "参数获取失败，请检测网络，或者脚本已经失效。"
+        exit 0
+    fi
+# 检测参数是否成功获取结束
 
 # 登录请求发送
     curl -H "User-Agent:${user_agent}" "http://${hostname}:${hostport}/eportal/?c=ACSetting&a=Login&protocol=http:&hostname=${hostname}&iTermType=1&wlanuserip=${userip}&wlanacip=${wlanacip}&wlanacname=${wlanacname}&mac=00-00-00-00-00-00&ip=${userip}&enAdvert=0&queryACIP=0&loginMethod=1" --data "DDDDD=${login_id}&upass=${login_password}&R1=0&R2=0&R3=0&R6=0&para=00&0MKKey=123456&buttonClicked=&redirect_url=&err_flag=&username=&password=&user=&cmd=&Login=&v6ip=" >/dev/null 2>/dev/null
